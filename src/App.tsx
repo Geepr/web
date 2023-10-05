2d1711dc5e14fb9e8b0abec8ef04f47b68f533d5
Piotr Kowalski <piotr@pkowalski.net>
2023-10-05 20:14:42 +0200

    Prepare base file structure including storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
+import "bootstrap/dist/css/bootstrap.css"
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './NavBar';
+import {NavBar} from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
+      <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
       </header>
-    </div>
+      <div className="container">
+        <main role="main">
+          <p>Hello!</p>
+        </main>
+      </div>
+    </>
   );
 }
 
