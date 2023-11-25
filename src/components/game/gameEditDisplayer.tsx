18971de415ec6de09eca3a3f566678ba1ba4d83a
Piotr Kowalski <piotr@pkowalski.net>
2023-11-25 10:39:40 +0100

    Add way to navigate to and from edit view

--- src/components/game/gameEditDisplayer.tsx
+++ src/components/game/gameEditDisplayer.tsx
@@ -1,16 +1,26 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {useParams, useNavigate} from "react-router-dom";
 import {useGameFetch} from "../../clients/gameClient";
 import GameEditForm from "./gameEditForm";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faAngleLeft} from "@fortawesome/free-solid-svg-icons/faAngleLeft";
 
 export default function GameEditDisplayer() {
     const {gameId} = useParams();
     const {game, loading} = useGameFetch(gameId!);
+    const navigate = useNavigate();
 
     if (loading)
         return <div>Loading...</div>
     if (game == null)
         return <div>Game not found...</div>
 
-    return <GameEditForm game={game}/>
+    return <>
+        <div className='row mb-3'>
+            <div className='col-1'>
+                <button className='btn btn-dark d-flex' onClick={() => navigate(-1)}><FontAwesomeIcon className='d-flex flex-fill' icon={faAngleLeft}/></button>
+            </div>
+        </div>
+        <GameEditForm game={game}/>
+    </>
 }
