d4bd3ec9c0400d4c4b8d5ec5d6300b2a486b057c
Piotr Kowalski <piotr@pkowalski.net>
2023-11-25 11:10:45 +0100

    Make game edit form look more pleasant
    Still has ways to go, but it exists in a usable way at least

--- src/components/game/gameEditForm.tsx
+++ src/components/game/gameEditForm.tsx
@@ -15,19 +15,49 @@ export default function GameEditForm(gameData : Readonly<GameEditFormData>) {
     const [formData, setFormData] = useState(new GameEditDto(gameData.game));
 
     async function handleSubmit(e : React.FormEvent) {
+        //todo: add feedback
         e.preventDefault();
         await submitGameEdit(gameData.game.id, formData);
     }
-    function handleInputChange (e: React.ChangeEvent<HTMLInputElement>) {
+    function handleInputChange (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) {
         const { name, value } = e.target;
         setFormData(prevState => ({ ...prevState, [name]: value }));
     }
+    function handleArchivedClick() {
+        setFormData(prevState => ({...prevState, archived: !prevState.archived}));
+    }
 
-    return <>
-        <form onSubmit={handleSubmit}>
-            <input type='text' name='title' value={formData?.title} onChange={handleInputChange}/>
-            <input type='hidden' name='archived' value='0'/>
-            <input type='submit' value='Save'/>
-        </form>
-    </>
+    return <div className='row'>
+        <div className='col-12'>
+            <form onSubmit={handleSubmit}>
+                <div className='row mb-2'>
+                    <div className='col-12 col-sm-2 my-auto'>
+                        <label htmlFor='title'>Title</label>
+                    </div>
+                    <div className='col-12 col-sm-10'>
+                        <input type='text' className='form-control' id='title' name='title' value={formData?.title} onChange={handleInputChange}/>
+                    </div>
+                </div>
+                <div className='row mb-2'>
+                    <div className='col-12 col-sm-2 my-auto'>
+                        <label htmlFor='description'>Description</label>
+                    </div>
+                    <div className='col-12 col-sm-10'>
+                        <textarea className='form-control' rows={5} id='description' name='description' value={formData?.description} onChange={handleInputChange}/>
+                    </div>
+                </div>
+                <div className='row mb-2 form-check'>
+                    <div className='col offset-sm-2'>
+                        <input className='form-check-input' type='checkbox' id='archived' name='archived' checked={formData.archived} onChange={handleArchivedClick} value='true'/>
+                        <label className='form-check-label' htmlFor='archived'>Archived</label>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='d-flex col-12 col-sm-2 offset-sm-10'>
+                        <input className='btn btn-primary flex-fill' type='submit' value='Save'/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 }
