8834e7f0fab514624258ca39bddd9c0596946cd0
Piotr Kowalski <piko98@proton.me>
2024-01-05 22:12:31 +0100

    Rewrite platform creator using dedicated form components

--- src/components/platform/platformCreator.tsx
+++ src/components/platform/platformCreator.tsx
@@ -3,27 +3,22 @@ import Creator, {CreatorChildData} from "../utils/creator";
 import PlatformCreateDto from "../../models/platformCreateDto";
 import {createPlatform} from "../../clients/platformClient";
 import {useNavigate} from "react-router-dom";
+import FormGroup from "../forms/formGroup";
+import FormLabel from "../forms/formLabel";
+import FormTextInput from "../forms/formTextInput";
 
 function platformCreatorFormControls(data : Readonly<CreatorChildData<PlatformCreateDto>>) {
     const {formData, handleInputChange} = data;
 
     return <>
-        <div className='row mb-2'>
-            <div className='col-12 col-sm-2 my-auto'>
-                <label htmlFor='name'>Name</label>
-            </div>
-            <div className='col-12 col-sm-10'>
-                <input type='text' maxLength={200} required={true} placeholder='Name of your new platform...' className='form-control' id='name' name='name' value={formData?.name} onChange={handleInputChange}/>
-            </div>
-        </div>
-        <div className='row mb-2'>
-            <div className='col-12 col-sm-2 my-auto'>
-                <label htmlFor='shortName'>Short name</label>
-            </div>
-            <div className='col-12 col-sm-10'>
-                <input type='text' maxLength={10} required={true} placeholder='Short name of your new platform...' className='form-control' id='shortName' name='shortName' value={formData?.shortName} onChange={handleInputChange}/>
-            </div>
-        </div>
+        <FormGroup>
+            <FormLabel forId={'name'}>Name</FormLabel>
+            <FormTextInput id={'name'} name={'name'} maxLength={200} required={true} placeholder={'Name of your new platform'} value={formData?.name} onChange={handleInputChange}/>
+        </FormGroup>
+        <FormGroup>
+            <FormLabel forId={'shortName'}>Short name</FormLabel>
+            <FormTextInput id={'shortName'} name={'shortName'} maxLength={10} required={true} placeholder={'Short name of your new platform...'} value={formData?.shortName} onChange={handleInputChange}/>
+        </FormGroup>
     </>
 }
 
