8afe410c683f8e39987ecd13102e3b889f6b0a42
Piotr Kowalski <piko98@proton.me>
2024-01-08 22:34:50 +0100

    Rewrite `platformClient` to use common components

---
+++ src/clients/commonClient.ts
@@ -0,0 +1,86 @@
+import {useEffect, useState} from "react";
+import {PaginationData, readPaginationDataFromJson} from "../components/pagination/paginationHelpers";
+import {validate as isValidUuid} from "uuid";
+import PlatformCreateDto from "../models/platformCreateDto";
+
+export interface ObjectFilter {
+    getQueryString() : string;
+}
+
+export function useObjectsFetch<T>(apiEndpoint : string, page : number, filter : ObjectFilter, readObjects : (json : any) => T[]) {
+    const [results, setResults] = useState<T[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [paginationData, setPaginationData] = useState<PaginationData>();
+
+    useEffect(() => {
+        async function load() {
+            try {
+                //todo: page size shouldn't be required here, really
+                const response = await fetch(`http://localhost:5510/api/${apiEndpoint}?page=${page}&pageSize=20&${filter.getQueryString()}`);
+                if (response.ok) {
+                    const json = await response.json();
+                    setPaginationData(readPaginationDataFromJson(json));
+                    setResults(readObjects(json));
+                } else {
+                    throw response;
+                }
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        load();
+    }, [page, filter, apiEndpoint, readObjects]);
+
+    return {results, loading, paginationData};
+}
+
+export function useObjectFetch<T>(apiEndpoint : string, uuid : string, readResult : (json : any) => T) {
+    const [result, setResult] = useState<T | undefined>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function load() {
+            try {
+                if (!isValidUuid(uuid))//todo: show error
+                    return;
+                const response = await fetch(`http://localhost:5510/api/${apiEndpoint}/${uuid}`);
+                if (response.ok) {
+                    const json = await response.json();
+                    setResult(readResult(json));
+                } else {
+                    throw response;
+                }
+            }
+            finally {
+                setLoading(false);
+            }
+        }
+        load();
+    }, [uuid, apiEndpoint, readResult]);
+
+    return {result, loading};
+}
+
+export async function submitObjectEdit<T>(endpoint : string, id : string, data : T) {
+    const response = await fetch(`http://localhost:5510/api/${endpoint}/${id}`, {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+    return response.ok;
+}
+
+export async function createObject<T>(endpoint : string, data : T) {
+    const response = await fetch(`http://localhost:5510/api/${endpoint}`, {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    });
+    if (response.ok) {
+        const json = await response.json();
+        return {success: true, id: json.id};
+    }
+    else
+        return {success: false, id : undefined};
+}
--- src/models/platformFilter.ts
+++ src/models/platformFilter.ts
@@ -1,3 +1,9 @@
-export default class PlatformFilter {
+import {ObjectFilter} from "../clients/commonClient";
+
+export default class PlatformFilter implements ObjectFilter{
     public name : string = ""
+
+    getQueryString(): string {
+        return `name=${this.name}`;
+    }
 }
